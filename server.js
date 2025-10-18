5a5befdb41a26bbf3314409e4bcbdf702e6f77f6
RajeshKumarStr <rajeshkumarstr02@gmail.com>
2025-10-18 13:58:19 +0530

    Initial commit

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
-      ? "https://rajopticals.vercel.app" 
-      : "http://localhost:5173",
+      ? "http://localhost:5173" 
+      : "https://rajopticals.vercel.app",
+    // process.env.NODE_ENV === "production"
+    //   ? "https://rajopticals.vercel.app" 
+    //   : "http://localhost:5173",
   methods: ["GET", "POST"],
   credentials: true,
 };
